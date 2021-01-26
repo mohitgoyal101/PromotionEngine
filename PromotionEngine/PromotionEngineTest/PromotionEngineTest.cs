c5c13d97c82f700e67e644f45d162fb8bfe43603
Mohit <Mohit@PERSONAL>
2021-01-26 14:22:59 +0530

    Intial Commit With Project File and Test Project

---
+++ PromotionEngine/PromotionEngineTest/PromotionEngineTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngineTest
+{
+    [TestClass]
+    public class PromotionEngineTest
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+
+        }
+
+        [TestMethod]
+        public void Scenario1_ApplyPromotion_ReturnTotal()
+        {
+        }
+    }
+}
