78d4655b92ebd5d5081a72eee825299d5b115354
Mohit <Mohit@PERSONAL>
2021-01-26 14:30:25 +0530

    Initial Cart Object Creation

---
+++ PromotionEngine/PromotionEngine/Model/SKU.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model
+{
+    public class SKU
+    {
+        public string Id { get; set; }
+    }
+
+    public class CartSKU : SKU
+    {
+        public int Quantity { get; set; }
+
+        public double DiscountedPrice { get; set; }
+
+        public bool IsPromotionApplied { get; set; }
+    }
+}
--- PromotionEngine/PromotionEngineTest/PromotionEngineTest.cs
+++ PromotionEngine/PromotionEngineTest/PromotionEngineTest.cs
@@ -1,26 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Model;
 
 namespace PromotionEngineTest
 {
     [TestClass]
     public class PromotionEngineTest
     {
+        Dictionary<SKU, double> priceList = null;
+
         [TestInitialize]
         public void Init()
         {
-            
+            //This should come from backend. For now initialising here for running test cases
+            priceList = new Dictionary<SKU, double>();
+            priceList.Add(new SKU() { Id = "A" }, 50);
+            priceList.Add(new SKU() { Id = "B" }, 30);
+            priceList.Add(new SKU() { Id = "C" }, 20);
+            priceList.Add(new SKU() { Id = "D" }, 15);
         }
 
         [TestCleanup]
         public void TestClean()
         {
-
         }
 
         [TestMethod]
         public void Scenario1_ApplyPromotion_ReturnTotal()
         {
+            //Prepare
+            Cart cart = new Cart()
+            {
+                CartSKUs = new List<CartSKU>()
+                {
+                    new CartSKU()
+                    {
+                        Id="A",
+                        Quantity=1
+                    },
+                     new CartSKU()
+                    {
+                        Id="B",
+                        Quantity=1
+                    },
+                      new CartSKU()
+                    {
+                        Id="C",
+                        Quantity=1
+                    },
+                }
+            };
+
+            //Act
+
+            //Assert
         }
     }
 }
